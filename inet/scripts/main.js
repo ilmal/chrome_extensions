8e0ad529c89f1427a3b408ee10f3553b08e922d3
ilmal <nils@u1.se>
2023-01-22 21:35:36 +0100

    adding new version of package

--- inet/scripts/main.js
+++ inet/scripts/main.js
@@ -36,7 +36,9 @@ const main = (msg) => {
 
             if (percent > 10 && percent < 25) tag.setAttribute("style", "color:red; font-size:2em; margin-right: 1rem;");
 
-            if (percent > 25) tag.setAttribute("style", "color:darkgoldenrod; font-size:2em; margin-right: 1rem;");
+            if (percent > 25 && percent < 50) tag.setAttribute("style", "color:darkgoldenrod; font-size:2em; margin-right: 1rem;");
+
+            if (percent > 50) tag.setAttribute("style", "background-color: #f3ec78; background-image: linear-gradient(45deg, #f3ec78, #e11e9a); background-size: 100%; -webkit-background-clip: text; -moz-background-clip: text; -webkit-text-fill-color: transparent; -moz-text-fill-color: transparent; ;font-size:2em; margin-right: 1rem;");
 
             tag.appendChild(text)
 
