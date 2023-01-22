04ddeb81098cf71e61a59c26e556691376eb0f01
ilmal <nils@u1.se>
2023-01-22 15:13:33 +0100

    adding v1

--- inet/scripts/main.js
+++ inet/scripts/main.js
@@ -1,46 +1,47 @@
-const main = ()=>{
+const main = () => {
     const divs = document.getElementsByClassName("s1crniwd")
 
-    //console.log(divs)
-
-    for (const e in divs){
+    for (const e in divs) {
 
         prices = divs[e].innerText
 
-        console.log(prices)
-        
-        price1 = parseInt(prices.split("\n")[0].replace(/\D/g,''))
-        price2 = parseInt(prices.split("\n")[1].replace(/\D/g,''))
+        if (prices == undefined) continue
 
-        //console.log(price1, price2)
+        if (divs[e].previousSibling != null) continue
 
-        const percent = (((1 - (price2 / price1)) * 100).toFixed(2)).toString() + "%"
+        price1 = parseInt(prices.split("\n")[0].replace(/\D/g, ''))
+        price2 = parseInt(prices.split("\n")[1].replace(/\D/g, ''))
 
-        //console.log(percent)
+        const percent = parseFloat(((1 - (price2 / price1)) * 100).toFixed(2))
+        const percent_string = percent.toString() + "%"
 
         const tag = document.createElement("span")
-        const text = document.createTextNode(percent)
+        const text = document.createTextNode(percent_string)
 
-        tag.setAttribute("style", "color:green; font-size:2em; margin-right: 1rem;")
+        if (percent < 5) tag.setAttribute("style", "color:green; font-size:2em; margin-right: 1rem;");
+
+        if (percent > 5 && percent < 10) tag.setAttribute("style", "color:fuchsia; font-size:2em; margin-right: 1rem;");
+
+        if (percent > 10 && percent < 25) tag.setAttribute("style", "color:red; font-size:2em; margin-right: 1rem;");
+
+        if (percent > 25) tag.setAttribute("style", "color:darkgoldenrod; font-size:2em; margin-right: 1rem;");
 
         tag.appendChild(text)
-
-        //console.log(typeof divs[e])
 
         divs[e].parentNode.insertBefore(tag, divs[e])
 
     }
 }
 
-const init = async ()=> {
+const init = async () => {
 
-    const sleep = (ms)=>{
+    const sleep = (ms) => {
         return new Promise(resolve => setTimeout(resolve, ms))
     }
 
-    while (true){
+    while (true) {
         main()
-        await sleep(10000)
+        await sleep(500)
     }
 }
 
